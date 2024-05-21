ef261505f5cafc6579664ef7fae9bbe468ad08e8
Liwen Ouyang <ouyang12359@gmail.com>
2024-05-21 15:56:50 -0700

    added hw6 starter code

---
+++ hw6/hw6.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=gpu-turing
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4
+
+### ---------------------------------------
+### BEGINNING OF EXECUTION
+### ---------------------------------------
+
+echo "Starting at `date`"
+echo
+make
+
+echo
+echo Output from main_q1
+echo ----------------
+mpirun -n 1 ./main_q1
+# mpirun -n 2 ./main_q1
+# mpirun -n 3 ./main_q1
+# mpirun -n 4 ./main_q1
